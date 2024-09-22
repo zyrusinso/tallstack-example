0f8382200050300e154463db32a9e64b8a7c38dc
zyrusinso <zyrusinso@gmail.com>
2024-09-22 14:13:47 +0800

    Fixing Dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('homepage');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
